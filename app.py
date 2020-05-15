b483b4c9860d4c05685f5764687c20683711fde4
vmanav <zmanav.1999@gmail.com>
2020-05-15 12:50:05 +0530

    Added JS compatibility for Camera and Speech Output

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, jsonify
 import Captions
+import json
+import base64
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return render_template("index.html")
 
-@app.route("/", methods = ["POST"])
+
+@app.route("/", methods=["POST"])
 def img():
     if request.method == "POST":
-        file = request.files["userimage"]
-        path = "static/{}".format(file.filename)
-        file.save(path)
 
+        data_url = request.form['data']
+        # print("data_url => ", data_url)
+        content = data_url.split(';')[1]
+        image_encoded = content.split(',')[1]
+
+        with open("imageToSave.png", "wb") as fh:
+            fh.write(base64.decodebytes(image_encoded.encode()))
+
+        path = "imageToSave.png"
         caption = Captions.caption_image(path)
 
-        result = {
-            "image": path,
-            "caption": caption
-        }
-    return render_template("index.html", result = result)
+        print("Generated Caption : ")
+        print(caption)
+
+        # result = {
+        #     "image": path,
+        #     "caption": caption
+        # }
+
+        return jsonify(
+            caption=caption
+        )
+
+    # return render_template("index.html", result=json.dumps(result))
+
 
 if __name__ == "__main__":
-    app.run(debug = True, threaded = False)
+    app.run(debug=True, threaded=False)
---
+++ static/script.js
@@ -0,0 +1,112 @@
+
+$(() => {
+
+    const startCameraButton = $('#sCamera');
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const captureButton = document.getElementById('capture');
+    const mainContainer = $('#mainContainer');
+
+
+    mainContainer.on("click", () => {
+        // console.log("Inside startCameraButton.Click()");
+        // // make videoChat visible
+        // vChatWindow.show();
+        mainContainer.off("click", () => {
+            alert("Hat gya Kya ?")
+        })
+
+
+        navigator.webkitGetUserMedia({
+            video: true,
+        }, function (stream) {
+
+            // attach your own stream
+            const videoElement = document.getElementById('videoStream')
+            // console.log("videoElement", videoElement);
+            videoElement.srcObject = stream;
+            videoElement.play()
+
+            // Text to Speech Output
+            function textToSpeechOutput(caption) {
+                var synth = window.speechSynthesis;
+                var utter = new SpeechSynthesisUtterance();
+                var textToBeSpoken = caption;
+
+                textToSpeech(textToBeSpoken);
+            }
+
+
+            function textToSpeech(textToBeSpoken) {
+
+                // get all voices that browser offers
+                var available_voices = window.speechSynthesis.getVoices();
+
+                // this will hold an english voice
+                var english_voice = '';
+
+                // find voice by language locale "en-US"
+                // if not then select the first voice
+                // console.log("available_voices", available_voices)
+
+                for (var i = 0; i < available_voices.length; i++) {
+                    if (available_voices[i].lang === 'en-US') {
+                        english_voice = available_voices[i];
+                        break;
+                    }
+                }
+                if (english_voice === '')
+                    english_voice = available_voices[0];
+
+                // new SpeechSynthesisUtterance object
+                var utter = new SpeechSynthesisUtterance();
+                utter.rate = 0.9;
+                utter.pitch = 0.3;
+                utter.text = textToBeSpoken;
+                utter.voice = english_voice;
+
+                // event after text has been spoken
+                utter.onend = function () {
+                    // alert('Speech has finished');
+                }
+                // speak
+                window.speechSynthesis.speak(utter);
+            }
+
+
+            function captureNewImage() {
+                context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+
+                let dataURL = canvas.toDataURL();
+                // console.log("DataURL => ", dataURL);
+
+                $.ajax({
+                    type: "POST",
+                    // contentType: "application/json",
+                    url: "/",
+                    data: { 'data': dataURL }
+                }).done(function (data) {
+                    console.log("Generated Caption :", data.caption);
+                    textToSpeechOutput(data.caption);
+
+                });
+
+            }
+
+            setInterval(() => {
+                captureNewImage();
+            }, 7000);
+
+        }, function (err) {
+            console.log("Error ->", err)
+            // console.log(err)
+            // console.log(err)
+            // console.log(err)
+            // console.log(err)
+            // console.log(err)   
+            // console.log(err)
+        })
+    })
+
+})
+
